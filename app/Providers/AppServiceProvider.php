3fd6539186fed6f1b88d7385a0666f8321d197d1
Fábio Rodrigues <fabiojob2023@gmail.com>
2023-10-17 22:04:51 -0300

    fix: Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(env('APP_ENV', 'production') == 'production') { 
-        URL::forceScheme('https')
-      }
+        if(env('APP_ENV', 'production') == 'production')
+        { 
+           URL::forceScheme('https');
+        }
     }
 }
